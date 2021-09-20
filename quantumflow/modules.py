8f3d24979e6f511f08f4030c085897bcea7d481b
gecrooks <gec@threeplusone.com>
2021-09-20 15:28:15 -0700

    Add DiagonalGate, MultiplexedGate, ConditionalGate, MultiplexedRzGate, and MultiplexedRyGate

--- quantumflow/modules.py
+++ quantumflow/modules.py
@@ -5,16 +5,21 @@
 
 """
 .. contents:: :local:
-.. currentmodule:: quantumcompiler
+.. currentmodule:: modules
 
-Gate Modules
-############
+Multi-qubit gates
+#################
 
-Larger unitary computational modules that
-can be broken up into standard gates.
+Larger unitary computational modules that can be broken up into standard gates.
 
-Danger: Gate's may have many qubits, so explicitly creating
-the gate tensor may consume huge amounts of memory. Beware.
+Danger: These multi-qubit gates have a variable, and possible large, number of qubits.
+Explicitly creating the gate tensor may consume huge amounts of memory. Beware.
+
+.. autoclass:: UnitaryGate
+    :members:
+
+.. autoclass:: DiagonalGate
+    :members:
 
 .. autoclass:: IdentityGate
     :members:
@@ -31,23 +36,35 @@ the gate tensor may consume huge amounts of memory. Beware.
 .. autoclass:: CircularShiftGate
     :members:
 
-.. autoclass:: ControlGate
-    :members:
-
 .. autoclass:: QFTGate
     :members:
 
 .. autoclass:: InvQFTGate
     :members:
 
+.. autoclass:: ControlGate
+    :members:
+
+.. autoclass:: MultiplexedGate
+    :members:
+
+.. autoclass:: ConditionalGate
+    :members:
+
+.. autoclass:: MultiplexedRzGate
+    :members:
+
 .. autoclass:: RandomGate
     :members:
 """
 
-from typing import Iterable, Iterator, List, Mapping, Union
+# TODO: Move all to gate module?
+
+from typing import Iterable, Iterator, List, Mapping, Sequence, Union, cast
 
 import networkx as nx
 import numpy as np
+import scipy
 from networkx.algorithms.approximation.steinertree import steiner_tree
 from sympy.combinatorics import Permutation
 
@@ -56,12 +73,28 @@ from .circuits import Circuit
 from .gates import unitary_from_hamiltonian
 from .ops import Gate, Operation, UnitaryGate
 from .paulialgebra import Pauli, pauli_commuting_sets, sX, sY, sZ
-from .qubits import Qubits
+from .qubits import Qubit, Qubits
 from .states import Density, State
-from .stdgates import CZ, CNot, CZPow
-from .stdgates import H as _H  # NB: Workaround for name conflict with Gate.H
-from .stdgates import I, Swap, X, XPow, Y, YPow, Z, ZPow
-from .tensors import QubitTensor
+from .stdgates import CZ, V_H, CNot, CZPow  # SqrtY,; SqrtY_H,
+from .stdgates import (
+    H as H_,
+)  # NB: Workaround for name conflict with Gate.H   # FIXME:  needed?
+from .stdgates import (  # SqrtY,; SqrtY_H,
+    I,
+    Ry,
+    Rz,
+    SqrtY,
+    SqrtY_H,
+    Swap,
+    V,
+    X,
+    XPow,
+    Y,
+    YPow,
+    Z,
+    ZPow,
+)
+from .tensors import QubitTensor, asqutensor
 from .var import Variable
 
 __all__ = (
@@ -70,9 +103,14 @@ __all__ = (
     "MultiSwapGate",
     "ReversalGate",
     "CircularShiftGate",
-    "ControlGate",
     "QFTGate",
     "InvQFTGate",
+    "DiagonalGate",
+    "ControlGate",
+    "MultiplexedGate",
+    "ConditionalGate",
+    "MultiplexedRzGate",
+    "MultiplexedRyGate",
     "RandomGate",
 )
 
@@ -191,7 +229,7 @@ class ControlGate(Gate):
 class MultiSwapGate(Gate):
     """A permutation of qubits. A generalized multi-qubit Swap."""
 
-    cv_tensor_structure = "permutation"
+    cv_tensor_structure = "swap"
     cv_hermitian = True
 
     def __init__(self, qubits_in: Qubits, qubits_out: Qubits) -> None:
@@ -217,7 +255,7 @@ class MultiSwapGate(Gate):
             elif isinstance(gate, I) or isinstance(gate, IdentityGate):
                 continue
             else:
-                raise ValueError("Swap gate must be built from swap gates")
+                raise ValueError("Swap gates must be built from swap gates")
 
         perm = list(range(N))
         for elem in circ:
@@ -335,12 +373,12 @@ class QFTGate(Gate):
     def H(self) -> "InvQFTGate":
         return InvQFTGate(self.qubits)
 
-    def decompose(self) -> Iterator[Union[_H, CZPow, Swap]]:
+    def decompose(self) -> Iterator[Union[H_, CZPow, Swap]]:
         qubits = self.qubits
         N = len(qubits)
         for n0 in range(N):
             q0 = qubits[n0]
-            yield _H(q0)
+            yield H_(q0)
             for n1 in range(n0 + 1, N):
                 q1 = qubits[n1]
                 yield CZ(q1, q0) ** (1 / 2 ** (n1 - n0))
@@ -364,7 +402,7 @@ class InvQFTGate(Gate):
     def H(self) -> "QFTGate":
         return QFTGate(self.qubits)
 
-    def decompose(self) -> Iterator[Union[_H, CZPow, Swap]]:
+    def decompose(self) -> Iterator[Union[H_, CZPow, Swap]]:
         gates = list(QFTGate(self.qubits).decompose())
         yield from (gate.H for gate in gates[::-1])
 
@@ -492,9 +530,333 @@ class PauliGate(Gate):
 
 # end class PauliGate
 
+# TODO: Move
+def merge_diagonal_gates(
+    gate0: "DiagonalGate", gate1: "DiagonalGate"
+) -> "DiagonalGate":
+    qubits = Circuit([gate0, gate1]).qubits
+    K = len(qubits)
+
+    extra_qubits0 = tuple(set(qubits) - set(gate0.qubits))
+    if extra_qubits0:
+        params = np.zeros(shape=[2] * K) + np.resize(
+            np.asarray(
+                gate0.params,
+            ),
+            [2] * gate0.qubit_nb,
+        )
+        gate0 = DiagonalGate(params.flatten(), extra_qubits0 + tuple(gate0.qubits))
+
+    gate0 = gate0.permute(qubits)
+
+    extra_qubits1 = tuple(set(qubits) - set(gate1.qubits))
+    if extra_qubits1:
+        params = np.zeros(shape=[2] * K) + np.resize(
+            np.asarray(
+                gate1.params,
+            ),
+            [2] * gate1.qubit_nb,
+        )
+        gate1 = DiagonalGate(params.flatten(), extra_qubits1 + tuple(gate1.qubits))
+    gate1 = gate1.permute(qubits)
+
+    params = (a + b for a, b in zip(gate0.params, gate1.params))
+    return DiagonalGate(params, qubits)
+
+
+class DiagonalGate(Gate):
+    r"""
+    A quantum gate whose unitary operator is diagonal in the computational basis.
+
+    .. math::
+        \text{DiagonalGate}(u_0, u_1, .., u_{n-1}) =
+            \begin{pmatrix}
+                \exp(-i h_0)     & 0       & \dots   & 0 \\
+                0       & \exp(-i h_1)     & \dots   & 0 \\
+                \vdots  & \vdots  & \ddots  & 0 \\
+                0       & 0       & \dots   & \exp(-i h_{n-1})
+            \end{pmatrix}
+
+    Diagonal gates can be decomposed in O(2^K) 2 qubit gates [1].
+
+    [1] Shende, Bullock, & Markov, Synthesis of Quantum Logic Circuits, 2006
+    IEEE Trans. on Computer-Aided Design, 25 (2006), 1000-1010
+    `arXiv:0406176 <https://arxiv.org/pdf/quant-ph/0406176.pdf>`_
+
+    """
+    cv_tensor_structure = "diagonal"
+
+    def __init__(
+        self, diag_hamiltonian: Union[np.ndarray, Sequence[Variable]], qubits: Qubits
+    ) -> None:
+        super().__init__(qubits=qubits, params=tuple(diag_hamiltonian))
+
+        assert len(self.params) == 2 ** self.qubit_nb  # FIXME
+
+    # DOCME TESTME
+    @classmethod
+    def from_gate(cls, gate: Gate) -> "DiagonalGate":
+        if isinstance(gate, DiagonalGate):
+            return gate
+
+        # Move elsewhere? gate_almost_diagonal?
+        def is_diagonal_gate(gate: Gate) -> bool:
+            if gate.cv_tensor_structure == "diagonal":
+                return True
+            if gate.cv_tensor_structure == "identity":
+                return True
+            return np.allclose(
+                np.diag(gate.tensor_diagonal.flatten()), gate.asoperator()
+            )
+
+        if not is_diagonal_gate(gate):
+            raise ValueError("Not a diagonal gate")
+
+        params = 1.0j * np.log(gate.tensor_diagonal.flatten())
+        return cls(list(params), gate.qubits)
+
+    # TESTME with symbolic
+    def permute(self, qubits: Qubits) -> "DiagonalGate":
+        """Permute the order of the qubits"""
+        qubits = tuple(qubits)
+        if self.qubits == qubits:
+            return self
+        params = np.resize(np.asarray(self.params), [2] * self.qubit_nb)
+        params = tensors.permute(params, self.qubit_indices(qubits))
+        return DiagonalGate(tuple(params.flatten()), qubits)
+
+    @utils.cached_property
+    def tensor(self) -> QubitTensor:
+        return asqutensor(np.diag(self.tensor_diagonal.flatten()))
+
+    @utils.cached_property
+    def tensor_diagonal(self) -> QubitTensor:
+        return asqutensor(np.exp(-1.0j * np.asarray(self.params)))
+
+    @property
+    def H(self) -> "DiagonalGate":
+        return self ** -1
+
+    def __pow__(self, e: Variable) -> "DiagonalGate":
+        params = [p * e for p in self.params]
+        return DiagonalGate(params, self.qubits)
+
+    def decompose(self, topology: nx.Graph = None) -> Iterator[Union[Rz, CNot]]:
+        diag_phases = self.params
+        N = self.qubit_nb
+        qbs = self.qubits
+
+        if N == 1:
+            yield Rz((diag_phases[0] - diag_phases[1]), qbs[0])
+        else:
+            phases = []
+            angles = []
+            for n in range(0, len(diag_phases), 2):
+                phases.append((diag_phases[n] + diag_phases[n + 1]) / 2)
+                angles.append(-(diag_phases[n + 1] - diag_phases[n]))
+
+            yield from MultiplexedRzGate(angles, qbs[:-1], qbs[-1]).decompose()
+            yield from DiagonalGate(phases, qbs[:-1]).decompose()
+
+
+# end class DiagonalGate
+
+
+# TODO: resolution of variables
+class MultiplexedGate(Gate):
+    """A uniformly controlled (or multiplexed) gate.
+
+    Given C control qubits and 2^C gate each acting on the same K target qubits,
+    return a multiplexed gate with C+K qubits. For each possible bitstring of the
+    control bits, a different gate is applied to the target qubits.
+    """
+
+    def __init__(self, gates: Sequence[Gate], controls: Qubits) -> None:
+        controls = tuple(controls)
+        gates = tuple(gates)
+        targets = gates[0].qubits
+        qubits = tuple(controls) + tuple(targets)
+        if len(set(qubits)) != len(qubits):
+            raise ValueError("Control and target qubits overlap")
+
+        for gate in gates:
+            if gate.qubits != targets:
+                raise ValueError("Target qubits of all gates must be the same.")
+
+        if len(gates) != 2 ** len(controls):
+            raise ValueError("Wrong number of target gates.")
+
+        super().__init__(qubits)
+        self.controls = controls
+        self.targets = targets
+        self.gates = gates
+
+    @utils.cached_property
+    def tensor(self) -> QubitTensor:
+        blocks = [gate.asoperator() for gate in self.gates]
+        return asqutensor(scipy.linalg.block_diag(*blocks))
+
+    @property
+    def H(self) -> "MultiplexedGate":
+        return self ** -1
+
+    def __pow__(self, e: Variable) -> "MultiplexedGate":
+        gates = [gate ** e for gate in self.gates]
+        return MultiplexedGate(gates, self.controls)
+
+    # TODO: deke to 2^N control gates
+
+    # Testme
+    def resolve(self, subs: Mapping[str, float]) -> "Gate":
+        gates = cast(Sequence[Gate], [gate.resolve(subs) for gate in self.gates])
+        return type(self)(gates, self.controls)
+
+
+# end class MultiplexedGate
+
+
+class ConditionalGate(MultiplexedGate):
+    """A conditional gate.
+
+    Perform gate A on the target qubit if the control qubit is zero,
+    else perform gate B. A multiplexed gate with only 1 control.
+    """
+
+    def __init__(self, A: Gate, B: Gate, control_qubit: Qubit) -> None:
+        super().__init__(gates=[A, B], controls=(control_qubit,))
+
+
+# end class ConditionalGate
+
+
+# FIXME: resolve wont work
+class MultiplexedRzGate(MultiplexedGate):
+    """Uniformly controlled (multiplexed) Rz gate"""
+
+    cv_tensor_structure = "diagonal"
+
+    def __init__(
+        self, thetas: Sequence[Variable], controls: Qubits, target: Qubit
+    ) -> None:
+
+        thetas = tuple(thetas)
+        gates = [Rz(theta, target) for theta in thetas]
+        super().__init__(gates=gates, controls=controls)
+        self._params = thetas  # FIXME: This seems broken?
+
+    @utils.cached_property
+    def tensor(self) -> QubitTensor:
+        return asqutensor(np.diag(self.tensor_diagonal.flatten()))
+
+    @utils.cached_property
+    def tensor_diagonal(self) -> QubitTensor:
+        diagonal = []
+        for theta in self.params:
+            rz = Rz(theta, 0)
+            diagonal.extend(np.diag(rz.tensor))
+
+        return asqutensor(diagonal)
+
+    @property
+    def H(self) -> "MultiplexedRzGate":
+        return self ** -1
+
+    def __pow__(self, e: Variable) -> "MultiplexedRzGate":
+        thetas = [e * p for p in self.params]
+        return MultiplexedRzGate(thetas, self.controls, self.targets[0])
+
+    def decompose(self, topology: nx.Graph = None) -> Iterator[Union[Rz, CNot]]:
+        thetas = self.params
+        N = self.qubit_nb
+        controls = self.controls
+        target = self.targets[0]
+
+        if N == 1:
+            yield Rz(thetas[0], target)
+        elif N == 2:
+            yield Rz((thetas[0] + thetas[1]) / 2, target)
+            yield CNot(controls[0], target)
+            yield Rz((thetas[0] - thetas[1]) / 2, target)
+            yield CNot(controls[0], target)
+        else:
+            # FIXME: Not quite optimal.
+            # There's additional cancellation of CNOTs that could happen
+            # See: From "Decomposition of Diagonal Hermitian Quantum Gates Using
+            # Multiple-Controlled Pauli Z Gates" (2014).
+
+            # Note that we lop off 2 qubits with each recursion.
+            # This allows us to cancel two cnots by reordering the second
+            # deke.
+
+            # If we lopped off one at a time the deke would look like this:
+            # t0 = thetas[0: len(thetas) // 2]
+            # t1 = thetas[len(thetas) // 2:]
+            # thetas0 = [(a + b) / 2 for a, b in zip(t0, t1)]
+            # thetas1 = [(a - b) / 2 for a, b in zip(t0, t1)]
+            # yield from MultiplexedRzGate(thetas0, qbs[1:]).decompose()
+            # yield CNot(qbs[0], qbs[-1])
+            # yield from MultiplexedRzGate(thetas1, qbs[1:]).decompose()
+            # yield CNot(qbs[0], qbs[-1])
+
+            M = len(thetas) // 4
+            quarters = list(thetas[i : i + M] for i in range(0, len(thetas), M))
+
+            theta0 = [(t0 + t1 + t2 + t3) / 4 for t0, t1, t2, t3 in zip(*quarters)]
+            theta1 = [(t0 - t1 + t2 - t3) / 4 for t0, t1, t2, t3 in zip(*quarters)]
+            theta2 = [(t0 + t1 - t2 - t3) / 4 for t0, t1, t2, t3 in zip(*quarters)]
+            theta3 = [(t0 - t1 - t2 + t3) / 4 for t0, t1, t2, t3 in zip(*quarters)]
+
+            yield from MultiplexedRzGate(theta0, controls[2:], target).decompose()
+            yield CNot(controls[1], target)
+            yield from MultiplexedRzGate(theta1, controls[2:], target).decompose()
+            yield CNot(controls[0], target)
+            yield from MultiplexedRzGate(theta3, controls[2:], target).decompose()
+            yield CNot(controls[1], target)
+            yield from MultiplexedRzGate(theta2, controls[2:], target).decompose()
+            yield CNot(controls[0], target)
+
+
+# end class MultiplexedRzGate
+
+
+class MultiplexedRyGate(MultiplexedGate):
+    """Uniformly controlled (multiplexed) Ry gate"""
+
+    def __init__(
+        self, thetas: Sequence[Variable], controls: Qubits, target: Qubit
+    ) -> None:
+
+        thetas = tuple(thetas)
+        gates = [Ry(theta, target) for theta in thetas]
+        super().__init__(gates=gates, controls=controls)
+        self._params = thetas  # FIXME: This seems broken?
+
+    @property
+    def H(self) -> "MultiplexedRyGate":
+        return self ** -1
+
+    def __pow__(self, e: Variable) -> "MultiplexedRyGate":
+        thetas = [e * p for p in self.params]
+        return MultiplexedRyGate(thetas, self.controls, self.targets[0])
+
+    def decompose(
+        self, topology: nx.Graph = None
+    ) -> Iterator[Union[V, V_H, MultiplexedRzGate]]:
+        thetas = self.params
+        controls = self.controls
+        target = self.targets[0]
+
+        yield V(target)
+        yield MultiplexedRzGate(thetas, controls, target)
+        yield V(target).H
+
+
+# end class MultiplexedRyGate
+
 
 class RandomGate(UnitaryGate):
     r"""Returns a random unitary gate acting on the given qubits.
+
     Ref:
         "How to generate random matrices from the classical compact groups"
         Francesco Mezzadri, math-ph/0609050
