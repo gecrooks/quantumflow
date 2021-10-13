8cd04a6137e0f1b606db1b001af9c10b80ac4394
gecrooks <gec@threeplusone.com>
2021-10-13 13:04:29 -0700

    Minor

--- quantumflow/future.py
+++ quantumflow/future.py
@@ -1,4 +1,4 @@
-# Copyright 2019-, Gavin E. Crooks
+# Copyright 2021-, Gavin E. Crooks
 #
 # This source code is licensed under the Apache-2.0 License
 # found in the LICENSE file in the root directory of this source tree.
--- quantumflow/stdgates.py
+++ quantumflow/stdgates.py
@@ -1,4 +1,4 @@
-# Copyright 2019-, Gavin E. Crooks
+# Copyright 2021-, Gavin E. Crooks
 #
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
