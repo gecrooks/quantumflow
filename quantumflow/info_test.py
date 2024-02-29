3813682ffb4774511facfbc37d964b3a7df6f5c0
gecrooks <gec@threeplusone.com>
2024-02-28 21:43:36 -0800

    .

--- quantumflow/info_test.py
+++ quantumflow/info_test.py
@@ -147,7 +147,7 @@ def test_purity() -> None:
 def test_bures_distance(repeat: int) -> None:
     rho = qf.random_density(4)
     # Note ATOL. Sometimes does not give accurate answer
-    assert np.isclose(qf.bures_distance(rho, rho), 0.0, atol=ATOL * 1000)
+    assert np.isclose(qf.bures_distance(rho, rho), 0.0,  atol=10**(-4))
 
     rho1 = qf.random_density(4)
     qf.bures_distance(rho, rho1)
@@ -170,7 +170,7 @@ def test_bures_angle() -> None:
     ang0 = qf.fubini_study_angle(ket0.tensor, ket1.tensor)
     ang1 = qf.bures_angle(rho0, rho1)
 
-    assert np.isclose(ang0, ang1, atol=ATOL*1000)
+    assert np.isclose(ang0, ang1,  atol=10**(-4))
 
 
 def test_entropy() -> None:
